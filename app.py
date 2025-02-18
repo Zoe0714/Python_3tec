d0615a6f99b4562b74d0c85147c0cad41c19a5a7
Zoe0714 <159541965+Zoe0714@users.noreply.github.com>
2025-02-18 09:53:57 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
-print('Lanches Paladar\n')
+import os
 
-print('1. Cadastrar lanchonete')
-print('2. Listar lanchonete')
-print('3. Ativar restaurante')
-print('4. Sair\n')
+def exibir_nome_do_programa():
+    print('Lanches Paladar\n')
+
+def exibir_opcoes()
+    print('1. Cadastrar lanchonete')
+    print('2. Listar lanchonete')
+    print('3. Ativar restaurante')
+    print('4. Sair\n')
 
 opcao_escolhida = int(input('Escolha uma opção: \n'))
 print(f'Você escolheu a opção {opcao_escolhida}\n')
 
 def finalizar_app():
+    os.system('cls')
     print('Finalizar o app')
 
 if opcao_escolhida == 1:
@@ -19,3 +24,10 @@ elif opcao_escolhida == 3:
     print('Ativar restaurante')
 else:
     finalizar_app()
+
+def main():
+    exibir_nome_do_programa()
+    exibir_opcoes()
+
+    if __name__ == '__main__':
+        main()
